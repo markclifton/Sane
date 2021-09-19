522801cc7903e5bd0a4fb5920123a266fb337583
Mark Clifton <m.clifton94@live.com>
2021-09-19 15:04:21 -0700

    Line ending bug

--- sane/entrypoint.hpp
+++ sane/entrypoint.hpp
@@ -10,8 +10,13 @@ extern Sane::App* CreateApp();
 int main(int argc, char* argv[])
 {
     std::string path(argv[0]);
+#if __APPLE__
     while (!path.empty() && path.back() != '/')
         path.pop_back();
+#else
+    while (!path.empty() && path.back() != '\\')
+        path.pop_back();
+#endif
 
     std::filesystem::current_path(path);
 
