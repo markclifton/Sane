3d622ee420df73330838cb0dde2847c7575040df
Mark Clifton <m.clifton94@live.com>
2021-09-19 13:57:20 -0700

    Additional Logging and fix CWD

--- sane/entrypoint.hpp
+++ sane/entrypoint.hpp
@@ -2,10 +2,19 @@
 
 #include <sane/core/app.hpp>
 
+#include <filesystem>
+#include <string>
+
 extern Sane::App* CreateApp();
 
 int main(int argc, char* argv[])
 {
+    std::string path(argv[0]);
+    while (!path.empty() && path.back() != '/')
+        path.pop_back();
+
+    std::filesystem::current_path(path);
+
     auto app = Sane::CreateApp();
     app->Run();
     delete app;
--- sane/graphics/buffer.cpp
+++ sane/graphics/buffer.cpp
@@ -1,10 +1,13 @@
 #include "sane/graphics/buffer.hpp"
 
+#include <sane/debugging/logging.hpp>
+
 namespace Sane
 {
   Buffer::Buffer(GLenum target) : target(target)
   {
     glGenBuffers(1, &buffer);
+    SANE_INFO("Successfully created buffer of type: " << target);
   }
 
   Buffer::~Buffer()
@@ -19,6 +22,8 @@ namespace Sane
 
   void Buffer::BufferData(GLsizeiptr size, const void* data, GLenum usage)
   {
+    glBindBuffer(target, buffer);
     glBufferData(target, size, data, usage);
+    SANE_INFO("Buffered data for buffer: " << buffer);
   }
 }
